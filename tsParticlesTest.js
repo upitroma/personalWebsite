eda72e1d93626365101ff5ecf604d23e672f5c76
upitroma <allen@rudolphweb.com>
2021-03-02 18:47:14 -0500

    upgraded to tsParticles.js

---
+++ tsParticlesTest.js
@@ -0,0 +1,29 @@
+// see https://www.skypack.dev/view/tsparticles for README
+
+tsParticles.load("tsparticles", {
+  fpsLimit: 60,
+  background: {
+    color: "#000000"
+  },
+  backgroundMode: {
+    enable: true
+  },
+  particles: {
+    color: {
+      value: "#ffffff"
+    },
+    links: {
+      color: "#ffffff",
+      enable: true
+    },
+    move: {
+      enable: true,
+      speed: 1
+    },
+    size: {
+      value: 2,
+      
+    },
+    
+  }
+});
